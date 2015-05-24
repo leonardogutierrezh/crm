92d856fca3bad10e8c8e214665312b2fe740feac
leonardo <leonardogutierrezh@gmail.com>
2015-05-24 13:50:09 -0430

    adelanto

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,10 +1,14 @@
 __author__ = 'leonardo'
 from django.conf.urls import patterns, include, url
-from .views import CreateContact, ListContact
+from .views import CreateContact, ListContact, ListContactList, ViewContact, CreateSingleContact
 
 urlpatterns = patterns('',
 
     url(r'^create/$', CreateContact.as_view(), name='contact_create'),
+    url(r'^create_single/$', CreateSingleContact.as_view(), name='contact_create_single'),
     url(r'^list/$', ListContact.as_view(), name='contact_list'),
+    url(r'^list_single/$', ListContactList.as_view(), name='list_contact_list'),
+    url(r'^view/(?P<pk>\d+)$', ViewContact.as_view(), name='view_contact'),
+
 
 )
--- contacts/views.py
+++ contacts/views.py
@@ -16,11 +16,25 @@ class CreateContact(generic.CreateView):
         Returns the supplied URL.
         """
         print self.object.pk
-        url = '/case/create_case/' + str(self.object.pk)
+        url = '/case/create/' + str(self.object.pk)
         return url
 
+class CreateSingleContact(generic.CreateView):
+    model = Contact
+    form_class = ContactForm
+    template_name = "create_contact.html"
+    success_url = "/contact/list_single/"
+
+class ViewContact(generic.DetailView):
+    model = Contact
+    template_name = "view_contact.html"
 
 class ListContact(generic.ListView):
     model = Contact
     template_name = 'list_contact.html'
+    template_name_suffix = 'contact_list'
+
+class ListContactList(generic.ListView):
+    model = Contact
+    template_name = 'list_contact_list.html'
     template_name_suffix = 'contact_list'
