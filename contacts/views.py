fb072b8ca8565a5942d69112fbc2b00ee2e696c0
leonardo <leonardogutierrezh@gmail.com>
2015-05-30 10:36:43 -0430

    Cambio en caracteres maximos de notas y editar contactos

--- contacts/views.py
+++ contacts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Contact
 from .forms import ContactForm
-
+from django.core.urlresolvers import reverse, reverse_lazy
 
 # Create your views here.
 class CreateContact(generic.CreateView):
@@ -38,3 +38,9 @@ class ListContactList(generic.ListView):
     model = Contact
     template_name = 'list_contact_list.html'
     template_name_suffix = 'contact_list'
+
+class EditContact(generic.UpdateView):
+    model = Contact
+    form_class = ContactForm
+    template_name = 'create_contact.html'
+    success_url = reverse_lazy('list_contact_list')
